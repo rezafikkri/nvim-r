8d0f80864e28f99e041f71cba20b9149dfc73583
rezafikkri <fikkri.reza@gmail.com>
2024-05-23 20:39:32 +0700

    feat: change leader key to <Space> and add kemaps for move text up and down

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -4,28 +4,42 @@ local opts = {
   silent = true,       -- do not show message
 }
 
+-- Shorten function name
+local keymap = vim.keymap.set
+
+-- change leader key to <Space>
+vim.g.mapleader = " "
+
 -----------------
 -- Normal mode --
 -----------------
 
 -- Hint: see `:h vim.map.set()`
 -- Better window navigation
-vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
-vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
-vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
-vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
+keymap('n', '<C-h>', '<C-w>h', opts)
+keymap('n', '<C-j>', '<C-w>j', opts)
+keymap('n', '<C-k>', '<C-w>k', opts)
+keymap('n', '<C-l>', '<C-w>l', opts)
 
 -- Resize with arrows
 -- delta: 2 lines
-vim.keymap.set('n', '<C-Up>', ':resize -2<CR>', opts)
-vim.keymap.set('n', '<C-Down>', ':resize +2<CR>', opts)
-vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', opts)
-vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', opts)
+keymap('n', '<C-Up>', ':resize -2<CR>', opts)
+keymap('n', '<C-Down>', ':resize +2<CR>', opts)
+keymap('n', '<C-Left>', ':vertical resize -2<CR>', opts)
+keymap('n', '<C-Right>', ':vertical resize +2<CR>', opts)
+
+-- Move text up and down
+keymap("n", "<A-j>", ":m .+1<CR>==", opts)
+keymap("n", "<A-k>", ":m .-2<CR>==", opts)
 
 -----------------
 -- Visual mode --
 -----------------
 
 -- Hint: start visual mode with the same area as the previous area and the same mode
-vim.keymap.set('v', '<Tab>', '>', opts)
-vim.keymap.set('v', '<S-Tab>', '<', opts)
+keymap('v', '<Tab>', '>', opts)
+keymap('v', '<S-Tab>', '<', opts)
+
+-- Move text up and down
+keymap("v", "<A-j>", ":m '>+1<CR>gv=gv", opts)
+keymap("v", "<A-k>", ":m '<-2<CR>gv=gv", opts)
