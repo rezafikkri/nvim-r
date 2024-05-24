25519d800cd7cb1187cc2c4b801d93bec43c3501
rezafikkri <fikkri.reza@gmail.com>
2024-05-24 10:09:58 +0700

    feat: install colorscheme and rename plugins.lua to lazy-plug and settings lazy.nvim performance
    Performance settings for lazy.nvim is disable unused rtp plugin

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
 require('options')
 require('keymaps')
-require('plugins')
+require('lazy-plug')
+require('ui')
---
+++ lua/lazy-plug.lua
@@ -0,0 +1,45 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  spec = {
+    { import = "plugins" },
+  },
+  default = {
+    lazy = true,
+  },
+  ui = {
+    border = "rounded",
+    -- The backdrop opacity. 0 is fully opaque, 100 is fully transparent.
+    backdrop = 90,
+  },
+  checker = {
+    -- automatically check for plugin updates
+    enabled = true,
+  },
+  performance = {
+    rtp = {
+      ---@type string[] list any plugins you want to disable here
+      disabled_plugins = {
+        "gzip",
+        "matchit",
+        "matchparen",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
