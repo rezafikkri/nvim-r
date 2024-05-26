5d69fa50cd4fbf7a067a51d96839fc570c0bcb32
rezafikkri <fikkri.reza@gmail.com>
2024-05-26 20:02:56 +0700

    feat: custom color for current number tokyonight theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -3,4 +3,15 @@ return {
   lazy = false, -- make sure we load this during startup if it is your main colorscheme
   priority = 1000, -- make sure to load this before all the other start plugins
   opts = {},
+  config = function ()
+    require("tokyonight").setup({
+      on_highlights = function(hl, c)
+        -- Change current line number color
+        hl.CursorLineNr = {
+          bold = true,
+          fg = "#6772A2"
+        }
+      end
+    })
+  end
 }
