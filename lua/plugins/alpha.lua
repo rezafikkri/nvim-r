3321bee78235faba72d9c8a9c55a77160e4b8372
rezafikkri <fikkri.reza@gmail.com>
2024-06-01 06:40:21 +0700

    feat: install and config nvim-ufo and change icons for dashboard

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -16,10 +16,10 @@ return {
       "    █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█ █▄█▄▄▄█    ",
     }
     dashboard.section.buttons.val = {
-      dashboard.button("f", "󰈞  Find file", ":Telescope find_files <CR>"),
-      dashboard.button("t", "󰊄  Find text", ":Telescope live_grep <CR>"),
+      dashboard.button("f", "󰭎  Find file", ":Telescope find_files <CR>"),
+      dashboard.button("t", "󱎸  Find text", ":Telescope live_grep <CR>"),
       dashboard.button("z", "󰒲  Lazy", ":Lazy<CR>"),
-      dashboard.button("q", "󰅚  Quit Neovim", ":qa<CR>"),
+      dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
     }
 
     alpha.setup(dashboard.config)
---
+++ lua/plugins/nvim-ufo.lua
@@ -0,0 +1,39 @@
+return {
+  "kevinhwang91/nvim-ufo",
+  dependencies = {
+    "kevinhwang91/promise-async",
+    {
+      "luukvbaal/statuscol.nvim",
+      config = function ()
+        local builtin = require("statuscol.builtin")
+        require("statuscol").setup {
+          relculright = true,
+          segments = {
+            {text = {"%s"}, click = "v:lua.ScSa"},
+            {text = {builtin.foldfunc, " "}, click = "v:lua.ScFa"},
+            {text = {builtin.lnumfunc, " "}, click = "v:lua.ScLa"},
+          },
+        }
+      end
+    },
+  },
+  event = "BufReadPost",
+  config = function ()
+    vim.opt.foldcolumn = '1' -- '0' is not bad
+    vim.opt.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+    vim.opt.foldlevelstart = 99
+    vim.opt.foldenable = true
+
+    vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+
+    -- Using ufo provider need remap `zR` and `zM`. If Neovim is 0.6.1, remap yourself
+    vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
+    vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
+
+    require('ufo').setup({
+      provider_selector = function()
+        return {'treesitter', 'indent'}
+      end
+    })
+  end
+}
