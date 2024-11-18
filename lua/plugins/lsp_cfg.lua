118fa05063b8cb3c1488dee0ac1fb204c172993e
rezafikkri <fikkri.reza@gmail.com>
2024-11-18 08:49:55 +0700

    feat: remove schemastore

--- lua/plugins/lsp_cfg.lua
+++ lua/plugins/lsp_cfg.lua
@@ -15,9 +15,6 @@ return {
       "LspInstall",
       "LspUninstall",
     },
-    dependencies = {
-      "b0o/schemastore.nvim",
-    },
     config = function()
       local sign = function(opts)
         -- See :help sign_define()
